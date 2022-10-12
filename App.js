698adff502f562592e6b46021c06138a6cde9eb5
Yongveloper <aaa9015@naver.com>
2022-10-12 23:04:25 +0900

    3.3 To Dos

--- App.js
+++ App.js
@@ -6,47 +6,69 @@ import {
   View,
   TouchableOpacity,
   TouchableHighlight,
+  TouchableWithoutFeedback,
   TextInput,
+  Keyboard,
 } from 'react-native';
 import { theme } from './color';
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState('');
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
 
   const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === '') {
+      return;
+    }
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText('');
+  };
+  console.log(toDos);
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.header}>
-        <TouchableOpacity onPress={work}>
-          <Text
-            style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}
-          >
-            Work
-          </Text>
-        </TouchableOpacity>
-        <TouchableHighlight onPress={travel}>
-          <Text
-            style={{
-              ...styles.btnText,
-              color: !working ? 'white' : theme.grey,
-            }}
-          >
-            Travel
-          </Text>
-        </TouchableHighlight>
+    // 빈공간 터치시 키보드 사라짐 (TouchableWithoutFeedback)
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.header}>
+          <TouchableOpacity onPress={work}>
+            <Text
+              style={{
+                ...styles.btnText,
+                color: working ? 'white' : theme.grey,
+              }}
+            >
+              Work
+            </Text>
+          </TouchableOpacity>
+          <TouchableHighlight onPress={travel}>
+            <Text
+              style={{
+                ...styles.btnText,
+                color: !working ? 'white' : theme.grey,
+              }}
+            >
+              Travel
+            </Text>
+          </TouchableHighlight>
+        </View>
+        <TextInput
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText}
+          returnKeyType="done"
+          value={text}
+          placeholder={working ? 'Add a To Do' : 'Where do you want to go?'}
+          style={styles.input}
+        />
       </View>
-      <TextInput
-        onChangeText={onChangeText}
-        value={text}
-        placeholder={working ? 'Add a To Do' : 'Where do you want to go?'}
-        style={styles.input}
-      />
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
